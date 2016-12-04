6c07a23d6f1585ff2124ccfa63644afbbb129bf0
Luiz Branco <me@luizbranco.com>
2016-12-03 21:20:19 -0500

    Solution for day 2 - part 2

--- day2.go
+++ day2.go
@@ -18,38 +18,62 @@ var pad = map[coordinates]string{
 	{1, -1}:  "9",
 }
 
-func BathroomCode(cmds string) string {
+var fancy = map[coordinates]string{
+	{0, 2}:   "1",
+	{-1, 1}:  "2",
+	{0, 1}:   "3",
+	{1, 1}:   "4",
+	{-2, 0}:  "5",
+	{-1, 0}:  "6",
+	{0, 0}:   "7",
+	{1, 0}:   "8",
+	{2, 0}:   "9",
+	{-1, -1}: "A",
+	{0, -1}:  "B",
+	{1, -1}:  "C",
+	{0, -2}:  "D",
+}
+
+func BathroomCode(cmds string, complex bool) string {
 	var k keypad
+	keys := pad
+
+	if complex {
+		keys = fancy
+		k.coordinates = coordinates{-2, 0}
+	}
 
 	code := ""
 
 	for _, line := range strings.Split(cmds, "\n") {
 		for _, c := range line {
-			k.move(c)
+			k.move(c, keys)
 		}
-		code += pad[k.coordinates]
+		code += keys[k.coordinates]
 	}
 
 	return code
 }
 
-func (k *keypad) move(direction rune) {
+func (k *keypad) move(direction rune, keys map[coordinates]string) string {
+	c := k.coordinates
+
 	switch direction {
 	case 'U':
-		if k.y < 1 {
-			k.y += 1
-		}
+		c.y += 1
 	case 'D':
-		if k.y > -1 {
-			k.y -= 1
-		}
+		c.y -= 1
 	case 'R':
-		if k.x < 1 {
-			k.x += 1
-		}
+		c.x += 1
 	case 'L':
-		if k.x > -1 {
-			k.x -= 1
-		}
+		c.x -= 1
 	}
+
+	btn, ok := keys[c]
+
+	if ok {
+		k.coordinates = c
+	}
+
+	return btn
 }
