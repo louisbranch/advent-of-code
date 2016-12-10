fa47649b24cc4b58cc11f7a77e0360c09bc2bb83
Luiz Branco <me@luizbranco.com>
2016-12-10 15:33:20 -0500

    Solution for day 5 - part 2

--- day5.go
+++ day5.go
@@ -27,3 +27,44 @@ func DoorPassword() {
 
 	fmt.Println(password)
 }
+
+func FancyDoorPassword() {
+	var password [8]*byte
+	i := 0
+
+	for {
+		h := md5.New()
+		io.WriteString(h, "ugkcyxxp")
+		io.WriteString(h, strconv.Itoa(i))
+		i++
+		hex := fmt.Sprintf("%0Xd", h.Sum(nil))
+
+		if strings.Index(hex, "00000") != 0 {
+			continue
+		}
+
+		p, err := strconv.Atoi(string(hex[5]))
+		if err != nil || p < 0 || p > 7 {
+			continue
+		}
+
+		v := hex[6]
+		if password[p] == nil {
+			password[p] = &v
+		}
+
+		done := true
+		for _, b := range password {
+			if b != nil {
+				fmt.Printf("%s", string(*b))
+			} else {
+				fmt.Print("_")
+				done = false
+			}
+		}
+		fmt.Println()
+		if done {
+			break
+		}
+	}
+}
