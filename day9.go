2af84f932d07d69030f48d2b495b2104976871e8
Luiz Branco <me@luizbranco.com>
2016-12-11 18:31:09 -0500

    Solution for day 9 - part 2

--- day9.go
+++ day9.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"log"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-func BulkDecompress(input string) int {
-	size := 0
+func BulkDecompress(input string) (v1, v2 int) {
 	for _, line := range strings.Split(input, "\n") {
-		dec := Decompress(line)
-		size += len(dec)
+		v1 += len(Decompress(line))
+		v2 += RecursiveDecompress(line)
 	}
-	return size
+	return
 }
 
 func Decompress(input string) string {
@@ -58,4 +58,32 @@ func Decompress(input string) string {
 	}
 
 	return dec
+}
+
+func RecursiveDecompress(input string) (length int) {
+	re := regexp.MustCompile(`^\((\d+)x(\d+)\)`)
+
+	for len(input) > 0 {
+		if matches := re.FindStringSubmatch(input); len(matches) == 3 {
+			size := len(matches[0])
+
+			offset, err := strconv.Atoi(matches[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			times, err := strconv.Atoi(matches[2])
+			if err != nil {
+				log.Fatal(err)
+			}
+			tmp := string(input[size : size+offset])
+
+			input = strings.Repeat(tmp, times) + string(input[size+offset:])
+		} else {
+			length++
+			input = input[1:]
+		}
+	}
+
+	return length
 }
