cfbd248cb1962d9160e9d62a8f0fd46498611a20
Luiz Branco <me@luizbranco.com>
2016-12-10 20:47:01 -0500

    Solution for day 7 - part 2

--- day7.go
+++ day7.go
@@ -51,3 +51,61 @@ func IpAbba(input string) int {
 
 	return count
 }
+
+func IpSSL(input string) int {
+	var c []rune
+	count := 0
+
+	for _, line := range strings.Split(input, "\n") {
+		var inner, outer [][]rune
+		inside := false
+
+		for _, char := range line {
+			switch char {
+			case '[':
+				c = c[:0]
+				inside = true
+				continue
+			case ']':
+				c = c[:0]
+				inside = false
+				continue
+			}
+
+			c = append(c, char)
+
+			if len(c) < 3 {
+				continue
+			}
+
+			if len(c) > 3 {
+				c = c[1:]
+			}
+
+			if c[0] != c[1] && c[0] == c[2] {
+				r := make([]rune, 3, 3)
+				copy(r, c)
+
+				if inside {
+					inner = append(inner, r)
+				} else {
+					outer = append(outer, r)
+				}
+			}
+		}
+
+	CHECK:
+		for _, out := range outer {
+			for _, in := range inner {
+				if in[0] == out[1] && in[1] == out[0] {
+					count++
+					break CHECK
+				}
+			}
+		}
+
+		c = c[:0]
+	}
+
+	return count
+}
